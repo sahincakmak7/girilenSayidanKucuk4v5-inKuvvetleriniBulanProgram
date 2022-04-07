a3bc17ea970ba07e959bd2903e24dc3e9c05761e
Şahin <sahincakmak7@gmail.com>
2022-04-07 19:36:50 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz :");
+        n = input.nextInt();
+
+        for(int i = 1; i <=n; i*=4){
+        System.out.println(i);
+        }
+        for(int i = 1; i <=n; i*=5){
+            System.out.println(i);
+        }
+
+    }
+}
